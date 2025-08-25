d894c1c03651dcae21b0fe56c7e28fc49dbf9359
luanmozer9 <luanmozer9@gmail.com>
2025-08-25 18:23:48 +0000

    INCLUSAO DE PIB PER CAPITA E DENSIDADE POPULACIONAL

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h> //Adicionado quando se usa (strcspn)
     int main() {
 
     printf("Desafio Super Trunfo - Países\n");
@@ -11,6 +12,8 @@
        float Area1;
        float PIB1;
        int Pontos1;
+       float Densidade1;
+       float Capita1;
 
        int Carta2;
        char Estado2[50];
@@ -20,55 +23,69 @@
        float Area2;
        float PIB2;
        int Pontos2;
+       float Densidade2;
+       float Capita2;
 
         printf("Digite o numero da carta1: \n");
         scanf("%d", &Carta1);
-
         printf("Digite o numero da carta2: \n");
         scanf("%d", &Carta2);
+        getchar();
 
         printf("Digite a sigla do estado1: \n");
-        scanf("%s", Estado1);
+        fgets(Estado1, sizeof(Estado1),stdin);
+        Estado1[strcspn(Estado1,"\n")]= '\0';
 
+        
         printf("Digite a sigla do estado2: \n");
-        scanf("%s", Estado2);
+        fgets(Estado2, sizeof(Estado2),stdin);
+        Estado2[strcspn(Estado2,"\n")]= '\0';
 
         printf("Digite o código do estado1: \n");
-        scanf("%s", Codigo1);
+        fgets(Codigo1, sizeof(Codigo1), stdin);
+        Codigo1[strcspn(Codigo1, "\n")] = '\0';
 
-        printf("Digite o codigo do estado2: \n");
-        scanf("%s", Codigo2);
+        printf("Digite o código do estado2: \n");
+        fgets(Codigo2, sizeof(Codigo2), stdin);
+        Codigo2[strcspn(Codigo2, "\n")] = '\0';
 
         printf("Digite o nome da cidade1: \n");
-        scanf("%s", Cidade1);
+        fgets(Cidade1, sizeof(Cidade1), stdin);
+        Cidade1[strcspn(Cidade1, "\n")] = '\0';
 
         printf("Digite o nome da cidade2: \n");
-        scanf("%s", Cidade2);
+        fgets(Cidade2, sizeof(Cidade2), stdin);
+        Cidade2[strcspn(Cidade2, "\n")] = '\0';
 
         printf("Qual a populacao1: \n");
         scanf("%d", &Populacao1);
-
         printf("Qual a populacao2: \n");
         scanf("%d", &Populacao2);
+        getchar();
+        
 
         printf("Qual a area do estado1: \n");
         scanf("%f", &Area1);
-
         printf("Qual a area do estado2: \n");
         scanf("%f", &Area2);
 
         printf("Qual o PIB da cidade1: \n");
         scanf("%f", &PIB1);
-
         printf("Qual o PIB da cidade2: \n");
         scanf("%f", &PIB2);
 
         printf("Quantos pontos turisticos possui a cidade1: \n");
         scanf("%d", &Pontos1);
-
         printf("Quantos pontos turisticos possui a cidade2: \n");
         scanf("%d", &Pontos2);
-        
+
+        Densidade1 = (float)(Populacao1 / Area1);
+        Densidade2 = (float)(Populacao2 / Area2);
+
+        Capita1 = (float)(PIB1 / Populacao1);
+        Capita2 = (float)(PIB2 / Populacao2);
+
+      
         printf("\n====Carta 1====\n");
         printf("Carta: %d \n", Carta1);
         printf("Estado: %s \n", Estado1);
@@ -78,6 +95,8 @@
         printf("Area: %f \n", Area1);
         printf("PIB: %f \n", PIB1);
         printf("Pontos turisticos: %d \n", Pontos1);
+        printf("Densidade populacional: %.2f \n", Densidade1);
+        printf("PIB per capita: %.2f \n", Capita1);
 
         printf("\n====Carta 2====\n");
         printf("Carta: %d \n", Carta2);
@@ -88,6 +107,8 @@
         printf("Area: %f \n", Area2);
         printf("PIB: %f \n", PIB2);
         printf("Pontos turisticos: %d \n", Pontos2);
+        printf("Densidade populacional: %.2f \n", Densidade2);
+        printf("PIB per capita: %.2f \n", Capita2);
     
 
 
